80638d01e6bd221b7f762159774615afed2c3d6f
Julio Telan <julio_telan@dlsu.edu.ph>
2015-04-23 18:08:57 +0800

    Added osd pool and rbd image verification

--- config_spec.rb
+++ config_spec.rb
@@ -9,9 +9,35 @@ describe service('tgt') do
   it { should be_running }
 end
 
+describe "Checking the pools" do
+  describe command('sudo ceph osd lspools') do
+    its(:stdout) {should match 'iscsi.1' }
+  end
+  describe command('sudo ceph osd lspools') do
+    its(:stdout) {should match 'iscsi.2' }
+  end
+  describe command('sudo ceph osd lspools') do
+    its(:stdout) {should match 'iscsi.2' }
+  end
+end
+
+describe "Checking the rbd images" do
+  describe command('sudo rbd ls iscsi.1') do
+    its(:stdout) {should match 'iscsi-rbd.1' }
+  end
+  describe command('sudo rbd ls iscsi.2') do
+    its(:stdout) {should match 'iscsi-rbd.2' }
+  end
+  describe command('sudo rbd ls iscsi.3') do
+    its(:stdout) {should match 'iscsi-rbd.3' }
+  end
+end
+
+
 describe port(3260) do
   it { should be_listening }
 end
+
 describe "Checking the files if copied" do
   describe command('ls /etc/tgt/conf.d/') do
     its(:stdout){ should match 'iscsi-1.conf' }
